8051eb56ce1eeacee377eb06800bf0a554cbeab2
Alita Joseph <alitajoseph@Alitas-MacBook-Air.local>
2015-10-07 13:54:39 -0500

    Palindrome

---
+++ Lab05/src/Palindrome.java
@@ -0,0 +1,29 @@
+/* -----------------------------------------------------------------------------
+ *
+ * File Name: Palindrome.java
+ * Author: Alita Joseph, a401j952@ku.edu
+ * Assignment:   EECS-168/169 Lab05
+ * Description:  The program finds the palindrome of an integer.The user is asked to enter the integer value
+ * Date: 10/07/15
+ ---------------------------------------------------------------------------- */
+import java.util.Scanner;
+public class Palindrome
+{
+public static void main(String[] args)
+{
+Scanner myScan = new Scanner(System.in);
+int number = 0;
+System.out.println("Please enter a number:");
+number = myScan.nextInt();
+}
+public static boolean isPalindrome(int number)
+{
+if(number )
+}
+public static int reverse(int x)
+{
+}
+public static int lengthOfNumber(int x)
+{
+}
+}
